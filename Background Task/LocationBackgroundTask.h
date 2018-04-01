81099bc12aff941e02871e00ed2d7b77758b6406
John Bosco Matanda <jbmatanda123@hotmail.com>
2018-04-01 14:09:06 +0200

    BackgroundTask classes done

---
+++ Background Task/LocationBackgroundTask.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#pragma once
+namespace Background_Task
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class LocationBackgroundTask sealed : public Windows::ApplicationModel::Background::IBackgroundTask
+	{
+	public:
+		LocationBackgroundTask();
+
+		virtual void Run(Windows::ApplicationModel::Background::IBackgroundTaskInstance^ taskInstance);
+
+	private:
+		void OnCanceled(Windows::ApplicationModel::Background::IBackgroundTaskInstance^ taskInstance, Windows::ApplicationModel::Background::BackgroundTaskCancellationReason reason);
+		~LocationBackgroundTask();
+
+		Concurrency::cancellation_token_source geopositionTaskTokenSource;
+	};
+}
+
